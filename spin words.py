21b1526c3e8fbaa956262872af6cfa9e8d70a963
ianbeltrao <74476396+ianbeltrao@users.noreply.github.com>
2021-10-26 11:37:57 +0100

    Delete spin words.py

--- spin words.py
+++
@@ -1,7 +0,0 @@
-def spin_words(sentence):
-    word_lst = sentence.split()
-    for i in range(len(word_lst)):
-        if len(word_lst[i])>4:
-            word_lst[i] = word_lst[i][::-1]
-    
-    return " ".join(word_lst)
